10397cb4336755a1dd7d0292546792834d06504b
JayBarton <damonjaybarton@gmail.com>
2024-04-04 23:33:32 -0400

    Moved text advancer into the scene manager

--- SceneManager.cpp
+++ SceneManager.cpp
@@ -10,14 +10,38 @@ SceneManager::SceneManager()
 	actions.push_back(0);
 	actions.push_back(1);
 	actions.push_back(2);
+	actions.push_back(3);
+}
+
+void SceneManager::init()
+{
+	playingScene = true;
+	testText.position = glm::vec2(100.0f, 100.0f);
+	testText.displayedPosition = testText.position;
+	testText.charsPerLine = 55;
+	testText.nextIndex = 55;
+
+	testText2.position = glm::vec2(100.0f, 400.0f);
+	testText2.charsPerLine = 55;
+	testText2.nextIndex = 55;
+	testText2.displayedPosition = testText2.position;
+
+	textManager.textLines.push_back({ 0, "Dad, it's already started!<0" });
+	textManager.textLines.push_back({ 1, "This is bad.<0" });
+	textManager.textLines.push_back({ 1, "Even as strong as Eyvale is,\nthese are imperial soldiers...Let's go.<0" });
+
+	textManager.textObjects.push_back(testText);
+	textManager.textObjects.push_back(testText2);
+	textManager.init();
 }
 
 void SceneManager::Update(float deltaTime, PlayerManager* playerManager, Camera& camera, InputManager& inputManager)
 {
 	if (state!= WAITING)
 	{
-		if (state == CAMERA_MOVE)
+		switch (state)
 		{
+		case CAMERA_MOVE:
 			if (camera.moving)
 			{
 				camera.MoveTo(deltaTime, 1.0f);
@@ -27,12 +51,11 @@ void SceneManager::Update(float deltaTime, PlayerManager* playerManager, Camera&
 				actionIndex++;
 				state = WAITING;
 			}
-		}
-		else if (state == UNIT_MOVE)
-		{
+			break;
+		case UNIT_MOVE:
 			if (activeUnit->movementComponent.moving)
 			{
-			//	activeUnit->UpdateMovement(deltaTime, inputManager);
+				//	activeUnit->UpdateMovement(deltaTime, inputManager);
 			}
 			else
 			{
@@ -42,6 +65,15 @@ void SceneManager::Update(float deltaTime, PlayerManager* playerManager, Camera&
 				actionIndex++;
 				state = WAITING;
 			}
+			break;
+		case TEXT:
+			textManager.Update(deltaTime, inputManager);
+			if (!textManager.active)
+			{
+				actionIndex++;
+				state = WAITING;
+			}
+			break;
 		}
 	}
 	else if (actionIndex < actions.size())
@@ -69,6 +101,11 @@ void SceneManager::Update(float deltaTime, PlayerManager* playerManager, Camera&
 			auto path = pathFinder.findPath(position, glm::vec2(64, 112), 99);
 			activeUnit->movementComponent.startMovement(path, 0, false);
 			state = UNIT_MOVE;
+		}
+		else if (currentAction == 3)
+		{
+			state = TEXT;
+			textManager.active = true;
 		}
 	}
 	else
--- SceneManager.h
+++ SceneManager.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include "PathFinder.h"
+#include "TextAdvancer.h"
+
 class Unit;
 class Camera;
 
@@ -8,7 +10,8 @@ enum SceneState
 {
 	WAITING,
 	CAMERA_MOVE,
-	UNIT_MOVE
+	UNIT_MOVE,
+	TEXT
 };
 struct SceneManager
 {
@@ -19,7 +22,13 @@ struct SceneManager
 	SceneState state = WAITING;
 	Unit* activeUnit = nullptr;
 
+	TextObject testText;
+	TextObject testText2;
+	TextObjectManager textManager;
+
 	SceneManager();
+
+	void init();
 
 	//Want this to be able to handle any unit manager, so might need to make that something that can be inherited.
 	void Update(float deltaTime, class PlayerManager* playerManager, Camera& camera, class InputManager& inputManager);
